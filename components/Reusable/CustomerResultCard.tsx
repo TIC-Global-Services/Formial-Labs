93435fd3050cd08e6e67016b9c56a9afa7f0da11
Manoj Kumar <manojkumararumainathan@gmail.com>
2026-09-08 17:37:22 +0530

    update: Customer card

--- components/Reusable/CustomerResultCard.tsx
+++ components/Reusable/CustomerResultCard.tsx
@@ -17,18 +17,18 @@ const CustomerResultCard = ({
   desc,
 }: CustomerResultsCardProps) => {
   return (
-    <div className="flex h-full flex-col overflow-hidden rounded-3xl bg-[#00476366] p-5 space-y-4">
+    <div className="flex h-full flex-col overflow-hidden rounded-4xl bg-[#00476366] p-3 space-y-4">
       <div className="relative aspect-4/3 w-full">
         <Image
           src={image}
           alt={`${name}'s skin transformation`}
           fill
           sizes="(min-width: 1024px) 35vw, (min-width: 640px) 50vw, 100vw"
-          className="object-cover rounded-2xl"
+          className="object-cover rounded-3xl"
         />
       </div>
 
-      <div className="flex flex-1 flex-col justify-center rounded-3xl bg-white p-6 sm:p-7">
+      <div className="flex flex-1 flex-col justify-center rounded-3xl bg-white p-6 sm:p-6">
         <div className="flex flex-nowrap items-center justify-between gap-x-3">
           <h3 className="min-w-0 truncate lg:text-xl font-medium text-primary sm:text-base">
             {name}, {location}
